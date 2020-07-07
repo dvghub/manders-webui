a04e7c8ac99a47c73e8555b7ad30df8658ddb582
dvghub <dieuwer.greevenbroek@gmail.com>
2020-07-07 10:53:25 +0200

    optimisations

--- src/views/agenda.vue
+++ src/views/agenda.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>suh</div>
+    <div class="card col-md-10 mx-auto my-5 border-0">
+        <h3 class="card-header bg-white">informatie</h3>
+        <div class="row card-body text-left">
+            <div class="col-12 col-md-7">
+
+            </div>
+            <div class="col">
+                double suh
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        today: new Date(),
+        day: 0,
+        month: this.today.getMonth() + 1,
+        year: this.today.getFullYear()
+      }
+    },
     name: "agenda"
   }
 </script>
